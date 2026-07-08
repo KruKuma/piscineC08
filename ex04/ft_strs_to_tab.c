dea7f2f44121f589bd9dacb06c18254bb2e5dad9
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-09 01:51:34 +0200

    feat(ex04): add stock

--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "ft_stock_str.h"
 
 int    ft_strlen(char *str)
 {
@@ -51,4 +52,39 @@ void    ft_free_tab(struct s_stock_str *tab, int index)
         i++;
     }
     free(tab);
+}
+
+int	ft_fill_tab(struct s_stock_str *tab, int ac, char **av)
+{
+	int	i;
+
+	i = 0;
+	while (i < ac)
+	{
+		tab[i].size = ft_strlen(av[i]);
+		tab[i].str = av[i];
+		tab[i].copy = ft_strdup(av[i]);
+		if (!tab[i].copy)
+		{
+			ft_free_tab(tab, i);
+			return (0);
+		}
+		i++;
+	}
+	tab[i].str = 0;
+	return (1);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	struct s_stock_str	*tab;
+
+	if (ac < 0)
+		return (0);
+	tab = malloc(sizeof(struct s_stock_str) * (ac + 1));
+	if (!tab)
+		return (0);
+	if (!ft_fill_tab(tab, ac, av))
+		return (0);
+	return (tab);
 }
