6acd56ad507cc54c1632065ca139d648274998e6
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-09 11:46:05 +0200

    fix(ex04): fix formatting for norminette

--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -6,52 +6,52 @@
 /*   By: nfurst <nfurst@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/08 16:08:35 by nfurst            #+#    #+#             */
-/*   Updated: 2026/07/08 16:08:38 by nfurst           ###   ########.fr       */
+/*   Updated: 2026/07/09 11:45:32 by nfurst           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "ft_stock_str.h"
 
-int    ft_strlen(char *str)
+int	ft_strlen(char *str)
 {
-    int	i;
+	int	i;
 
-    i = 0;
-    while (str[i])
-        i++;
-    return (i);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-char    *ft_strdup(char *src)
+char	*ft_strdup(char *src)
 {
-    char    *dest;
-    int        i;
+	char	*dest;
+	int		i;
 
-    dest = malloc(sizeof(char) * (ft_strlen(src) + 1));
-    if (!dest)
-        return (0);
-    i = 0;
-    while (src[i])
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
+	dest = malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (!dest)
+		return (0);
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
-void    ft_free_tab(struct s_stock_str *tab, int index)
+void	ft_free_tab(struct s_stock_str *tab, int index)
 {
-    int    i;
+	int	i;
 
-    i = 0;
-    while (i < index)
-    {
-        free(tab[i].copy);
-        i++;
-    }
-    free(tab);
+	i = 0;
+	while (i < index)
+	{
+		free(tab[i].copy);
+		i++;
+	}
+	free(tab);
 }
 
 int	ft_fill_tab(struct s_stock_str *tab, int ac, char **av)
