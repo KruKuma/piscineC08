f5922d954f323f4c2b56f2fde43702c67da0fbdf
Naphatsakorn Fuerst <nkfurst@gmail.com>
2026-07-10 14:00:51 +0200

    fix(ex01): add newline after message

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -20,8 +20,8 @@ typedef int	t_bool;
 # define TRUE 1
 # define FALSE 2
 # define EVEN(nbr) ((nbr) % 2 == 0)
-# define EVEN_MSG "I have an even number of arguments."
-# define ODD_MSG "I have an odd number of arguments."
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 # define SUCCESS 0
 
 #endif
